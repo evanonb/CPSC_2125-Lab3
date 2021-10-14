49c09b6a46f9fb186b11bb2c905ac6b57c39272d
Evan Owenby <89050989+evanonb@users.noreply.github.com>
2021-10-14 13:54:19 +0000

    added buttons for changing background color for article grid item

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function startup() {
 function lastUpdated() {
   document.getElementById("lastUpdatedDate").innerHTML = document.lastModified;
 }
+
+//scripts to change backgrond color for article grid item in lab 6
+function bkg-vi() {
+  document.getElementsByClassName("article").bkg = pink;
+}
+
+function bkg-mu() {
+
+}
+
+function bkg-inv() {
+
+}
